d303272f8adb331efd47e7f884ae000a030c85bc
Ch N V S S Durga Prasad <chodagiriprasad5@gmail.com>
2025-08-19 18:30:37 +0530

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
 import { createRoot } from 'react-dom/client'
-// ✅ Fix
-import helmetPkg from "react-helmet-async";
-const { HelmetProvider } = helmetPkg;
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(
-  <HelmetProvider>
     <App />
-  </HelmetProvider>
 )
