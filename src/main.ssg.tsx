550df9c0bff8d262ece7aa790b48c86735873540
Ch N V S S Durga Prasad <chodagiriprasad5@gmail.com>
2025-08-19 17:22:11 +0530

    commit

--- src/main.ssg.tsx
+++ src/main.ssg.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ViteSSG } from "vite-ssg";
+import helmetPkg from "react-helmet-async";
+const { HelmetProvider } = helmetPkg;
 import { BrowserRouter } from "react-router-dom";
+const isClient = typeof window !== "undefined";
 
 export const createApp = ViteSSG(
-  () => (
-    <BrowserRouter>
+  () =>
+    isClient ? (
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    ) : (
       <App />
-    </BrowserRouter>
-  ),
+    ),
   { routes: ["/"] }
 );
